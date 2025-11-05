e6fa2eefa5e45c23b863d7fa26f353a991b309ee
ashutoshsingh129 <ashutosh.singh@neosoftmail.com>
2025-11-05 23:42:19 +0530

    added form for uk

--- Client/src/components/DirectOnboardForm/DirectOnboardFormRouter.tsx
+++ Client/src/components/DirectOnboardForm/DirectOnboardFormRouter.tsx
@@ -3,6 +3,7 @@ import { DirectOnboardFormProps } from './types';
 import USAForm from './USA';
 import SwedenForm from './Sweden';
 import FranceForm from './France';
+import UKForm from './UK';
 
 const DirectOnboardFormRouter: React.FC<DirectOnboardFormProps> = props => {
     const country = (props.country || 'US').toUpperCase();
@@ -12,6 +13,9 @@ const DirectOnboardFormRouter: React.FC<DirectOnboardFormProps> = props => {
             return <SwedenForm {...props} />;
         case 'FR':
             return <FranceForm {...props} />;
+        case 'GB':
+        case 'UK':
+            return <UKForm {...props} />;
         case 'US':
         default:
             return <USAForm {...props} />;
